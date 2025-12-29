3c835c5cdd4c69e436074cea14115e39582e5447
SebasCodeDeveloper <sebatianpena950@gmail.com>
2025-12-29 01:05:12 -0500

    ajuste de Userservice

--- Crud_Project/src/main/java/com/proyec/crud/repository/UserRepository.java
+++ Crud_Project/src/main/java/com/proyec/crud/repository/UserRepository.java
@@ -5,12 +5,13 @@ package com.proyec.crud.repository;
 
 import com.proyec.crud.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-
     // Método para buscar un usuario por su email
     Optional<User> findByEmail(String email);
 }
